59d4ce26c9f000ebd35ca5abd254f6085e7a4ff2
Doug Neal <doug@neal.me.uk>
2017-04-18 12:15:42 +0100

    Populate init.pp, params.pp, go-audit.yaml.erb
    Build out the module parameters and config template, using go-audit's
    example config file as a source of defaults, and a basis for template
    structure

--- manifests/init.pp
+++ manifests/init.pp
@@ -43,11 +43,31 @@
 # Copyright 2017 Your name here, unless otherwise noted.
 #
 class goaudit (
-  $package_name   = $goaudit::params::package_name,
-  $package_ensure = $goaudit::params::package_ensure,
-  $config_file    = $goaudit::params::config_file,
-  $service_name   = $goaudit::params::service_name,
-  $service_ensure = $goaudit::params::service_ensure,
+  $package_name             = $goaudit::params::package_name,
+  $package_ensure           = $goaudit::params::package_ensure,
+  $config_file              = $goaudit::params::config_file,
+  $service_name             = $goaudit::params::service_name,
+  $service_ensure           = $goaudit::params::service_ensure,
+  $events_min               = $goaudit::params::events_min,
+  $events_max               = $goaudit::params::events_max,
+  $message_tracking_enabled = $goaudit::params::message_tracking_enabled,
+  $message_tracking_log_ooo = $goaudit::params::message_tracking_log_ooo,
+  $message_tracking_max_ooo = $goaudit::params::message_tracking_max_ooo,
+  $output_stdout_enabled    = $goaudit::params::output_stdout_enabled,
+  $output_stdout_attempts   = $goaudit::params::output_stdout_attempts,
+  $output_syslog_enabled    = $goaudit::params::output_syslog_enabled,
+  $output_syslog_attempts   = $goaudit::params::output_syslog_attempts,
+  $output_syslog_network    = $goaudit::params::output_syslog_network,
+  $output_syslog_address    = $goaudit::params::output_syslog_address,
+  $output_syslog_priority   = $goaudit::params::output_syslog_priority,
+  $output_syslog_tag        = $goaudit::params::output_syslog_tag,
+  $output_file_enabled      = $goaudit::params::output_file_enabled,
+  $output_file_attempts     = $goaudit::params::output_file_attempts,
+  $output_file_path         = $goaudit::params::output_file_path,
+  $output_file_mode         = $goaudit::params::output_file_mode,
+  $output_file_user         = $goaudit::params::output_file_user,
+  $output_file_group        = $goaudit::params::output_file_group,
+  $log_flags                = $goaudit::params::log_flags,
 ) inherits goaudit::params {
 
   validate_string($package_name)
@@ -55,6 +75,40 @@ class goaudit (
   validate_absolute_path($config_file)
   validate_string($service_name)
   validate_string($service_ensure)
+
+  validate_integer($events_min)
+  validate_integer($events_max)
+
+  validate_bool($message_tracking_enabled)
+  validate_bool($message_tracking_log_ooo)
+  validate_integer($message_tracking_max_ooo)
+
+  validate_bool($output_syslog_enabled)
+  validate_integer($output_syslog_attempts)
+  validate_string($output_syslog_network)
+  $valid_socket_types = [
+    'tcp', 'tcp4', 'tcp6', 'udp', 'udp4', 'udp6', 'ip',
+    'ip4', 'ip6', 'unix', 'unixgram', 'unixpacket',
+  ]
+  if (! $output_syslog_network in $valid_socket_types) {
+    fail(
+      sprintf('output_syslog_network must be one of %s',
+        join($valid_socket_types, ', ')
+      )
+    )
+  }
+  validate_absolute_path($output_syslog_address)
+  validate_integer($output_syslog_priority)
+  validate_string($output_syslog_tag)
+
+  validate_bool($output_file_enabled)
+  validate_integer($output_file_attempts)
+  validate_absolute_path($output_file_path)
+  validate_string($output_file_mode)
+  validate_string($output_file_user)
+  validate_string($output_file_group)
+
+  validate_integer($log_flags)
 
   anchor { 'goaudit::begin': } ->
   class { '::goaudit::install': } ->
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,6 +2,32 @@ class goaudit::params {
   $package_ensure = 'present'
   $service_ensure = 'running'
 
+  $events_min = 1300
+  $events_max = 1399
+
+  $message_tracking_enabled = true
+  $message_tracking_log_ooo = false
+  $message_tracking_max_ooo = 500
+
+  $output_stdout_enabled = true
+  $output_stdout_attempts = 2
+
+  $output_syslog_enabled = false
+  $output_syslog_attempts = 5
+  $output_syslog_network = 'unixgram'
+  $output_syslog_address = '/dev/log'
+  $output_syslog_priority = 129
+  $output_syslog_tag = 'go-audit'
+
+  $output_file_enabled = false
+  $output_file_attempts = 2
+  $output_file_path = '/var/log/go-audit/go-audit.log'
+  $output_file_mode = '0600'
+  $output_file_user = 'root'
+  $output_file_group = 'root'
+
+  $log_flags = 0
+
   case $::osfamily {
     'Debian': {
       $package_name = 'go-audit'
@@ -10,6 +36,7 @@ class goaudit::params {
     }
     default: {
       fail("The ${module_name} module is not supported on a ${::osfamily} based system.")
+    }
   }
 }
 
