c7cf9e86aeca0e148f040c491017fe7b14bde64c
Doug Neal <doug@neal.me.uk>
2017-04-20 13:24:56 +0100

    Make `content` parameter of goaudit::rule mandatory

--- manifests/rule.pp
+++ manifests/rule.pp
@@ -1,12 +1,15 @@
 define goaudit::rule (
   $order   = 10,
   $comment = undef,
-  $content = '',
+  $content,
 ) {
 
   validate_integer($order)
-  if defined($comment) {
+  if ($comment != undef) {
     validate_string($comment)
+  }
+  if ! (is_array($content) or is_string($content)) {
+    fail('content must be a string or an array of strings')
   }
 
   datacat_fragment {"go-audit rule ${title}" :
